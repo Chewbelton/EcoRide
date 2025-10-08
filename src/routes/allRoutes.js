c196ef88b92b0055d260326f11f23558168b21bd
Chewbelton <cagliero.baptiste@outlook.fr>
2025-10-08 19:20:17 +0200

    Création de la page 'Mon compte' et modifications mineures sur le css

--- src/routes/allRoutes.js
+++ src/routes/allRoutes.js
@@ -3,11 +3,13 @@ import Route from "./route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "./pages/home.html"),
-    new Route("/choix-trajet", "Covoiturages", "./pages/choix-trajet.html"),
-    new Route("/detail-trajet", "Détail du trajet", "./pages/detail-trajet.html"),
+    new Route("/connexion", "Connexion", "./pages/connexion.html"),
+    new Route("/inscription", "Inscription", "./pages/inscription.html"),
     new Route("/contacts", "Contacts", "./pages/contacts.html"),
-    new Route("/connexion", "Connexion", "./pages/connexion.html")
-  ];
+    new Route("/mon-compte", "Mon compte", "./pages/mon-compte.html"),
+    new Route("/choix-trajet", "Covoiturages", "./pages/choix-trajet.html"),
+    new Route("/detail-trajet", "Détail du trajet", "./pages/detail-trajet.html")
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "EcoRide";
